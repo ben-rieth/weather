28c31f1df984afad06621df97f8169385abefb48
ben-rieth <benrieth3@gmail.com>
2023-02-14 15:04:28 -0600

    Return weather data to frontend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,38 @@
-<script>
+<script lang='ts'>
+	import { PUBLIC_URL } from '$env/static/public';
+    import axios from 'axios';
     import '@picocss/pico';
+
+    let city = '';
+    let country = '';
+
+    let data: any | undefined;
+
+    const searchEndpoint = `${PUBLIC_URL}/api/weather/search`;
+
+    const onSubmit = async () => {
+        const result = await axios.get(searchEndpoint, {
+            params: {
+                city,
+                country
+            }
+        }).then(res => res.data);
+
+        data = result;
+    }
+
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<form on:submit|preventDefault={onSubmit}>
+    <input type='text' name='city' bind:value={city} placeholder='City*' />
+    <input type='text' name='country' bind:value={country} placeholder='Country'/>
+    <button type='submit'>Submit</button>
+</form>
+
+{#if data}
+    <p>{JSON.stringify(data)}</p>
+{/if}
+
