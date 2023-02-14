2eaba62af07ba74f9a4a8222c185c9381abdaab3
ben-rieth <benrieth3@gmail.com>
2023-02-14 17:20:19 -0600

    Get user location weather when button pushed

--- src/components/UserLocationButton/UserLocationButton.svelte
+++ src/components/UserLocationButton/UserLocationButton.svelte
@@ -1,11 +1,36 @@
 <script lang='ts'>
-    import { weatherStatus } from "../../stores/weather";
+	import { PUBLIC_URL } from "$env/static/public";
+	import axios from "axios";
+    import { weatherStatus, weatherData } from "../../stores/weather";
 
+    const coordsEndpoint = `${PUBLIC_URL}/api/weather/coords`
 
     const userLocation = async () => {
         $weatherStatus = 'loading';
 
-        $weatherStatus = 'success';
+        navigator.geolocation.getCurrentPosition(
+            async (pos) => {
+
+                try {
+                    const data = await axios.get(coordsEndpoint, {
+                        params: {
+                            lat: pos.coords.latitude,
+                            lon: pos.coords.longitude,
+                        }
+                    }).then(res => res.data);
+
+                    $weatherData = data;
+
+                    $weatherStatus = 'success';
+                } catch (err) {
+                    $weatherStatus = 'error';
+                }
+            },
+            () => {
+                console.log("Failure to get user coords")
+                $weatherStatus = 'error';
+            }
+        )
     }
 </script>
 
