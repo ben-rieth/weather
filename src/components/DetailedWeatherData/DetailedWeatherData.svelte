33f187b07b0f15f022a337428ad850ba2c9ef875
ben-rieth <benrieth3@gmail.com>
2023-02-14 22:24:22 -0600

    Add DetailedWeatherData component

---
+++ src/components/DetailedWeatherData/DetailedWeatherData.svelte
@@ -0,0 +1,22 @@
+<script lang='ts'>
+	import type { CurrentData } from "../../types/Weather";
+
+    export let data : CurrentData;
+</script>
+
+<div class="data-grid">
+    <p>Feels Like: {data.feelsLike}</p>
+    <p>Humidity: {data.humidity}</p>
+    <p>Wind Speed: {data.windSpeed}</p>
+    <p>Pressure: {data.pressure}</p>
+    <p>Sunrise: {data.sunriseTime}</p>
+    <p>Sunset: {data.sunsetTime}</p>
+</div> 
+
+<style>
+    .data-grid {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        max-width: 800px;
+    }
+</style>
--- src/components/WeatherData/WeatherData.svelte
+++ src/components/WeatherData/WeatherData.svelte
@@ -1,5 +1,6 @@
 <script lang='ts'>
 import { weatherData } from "../../stores/weather";
+	import DetailedWeatherData from "../DetailedWeatherData/DetailedWeatherData.svelte";
 
 </script>
 
@@ -13,18 +14,8 @@ import { weatherData } from "../../stores/weather";
                 <h2></h2>
             </hgroup>    
         </header>
-
-        <details open>
-            <summary>Detailed Current Weather</summary>
-                <div class="data-grid">
-                    <p>Feels Like: {$weatherData.current.feelsLike}</p>
-                    <p>Humidity: {$weatherData.current.humidity}</p>
-                    <p>Wind Speed: {$weatherData.current.windSpeed}</p>
-                    <p>Pressure: {$weatherData.current.pressure}</p>
-                    <p>Sunrise: {$weatherData.current.sunriseTime}</p>
-                    <p>Sunset: {$weatherData.current.sunsetTime}</p>
-                </div>
-        </details>      
+            
+        <DetailedWeatherData data={$weatherData.current} />
 
         <footer>
             <button>Save City</button>
@@ -38,11 +29,5 @@ import { weatherData } from "../../stores/weather";
         background-image: url('/images/clear-skies.jpg');
         background-size: cover;
         background-position: center;
-    }
-
-    .data-grid {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        max-width: 800px;
     }
 </style>
