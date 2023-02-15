f41931a0d9f5eb074f09bf410ebde233b4bddbfb
ben-rieth <benrieth3@gmail.com>
2023-02-14 23:17:12 -0600

    Get weather for saved cities

--- src/components/SavedCitiesDropdown/SavedCitiesDropdown.svelte
+++ src/components/SavedCitiesDropdown/SavedCitiesDropdown.svelte
@@ -1,6 +1,11 @@
 <script lang='ts'>
-    import { savedCities } from '../../stores/weather';
+    import { weatherData, weatherStatus, savedCities } from '../../stores/weather';
     import { onMount } from 'svelte';
+    import axios from 'axios';
+	import type { SavedCity } from '../../types/Geo';
+	import { PUBLIC_URL } from '$env/static/public';
+
+    const coordsEndpoint = `${PUBLIC_URL}/api/weather/coords`
 
     onMount(async () => {
         const _storageData = localStorage.getItem('saved');
@@ -9,13 +14,32 @@
             $savedCities = JSON.parse(_storageData);
         }
     });
+
+    const getWeather = async (city: SavedCity) => {
+        try {
+            const data = await axios.get(coordsEndpoint, {
+                params: {
+                    lat: city.lat,
+                    lon: city.lon,
+                }
+            }).then(res => res.data);
+
+            $weatherData = data;
+
+            $weatherStatus = 'success';
+        } catch (err) {
+            $weatherStatus = 'error';
+        }
+    };
 </script>
 
 <details role="list">
-    <summary aria-haspopup="listbox">Saved Cities</summary>
+    <summary aria-haspopup="listbox">Saved Cities&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</summary>
     <ul role="listbox">
         {#each $savedCities as city}
-            <li>{city.city}</li>
+            <li>
+                <a href="#top" on:click={() => getWeather(city)}>{city.city}</a>
+            </li>
         {/each}
 
         {#if $savedCities.length === 0}
